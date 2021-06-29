70a013b236f9dabbc428997b22339e1db36887aa
noriba.projects <noriba.projects@gmail.com>
2021-06-30 00:05:43 +0200

    entities timesheets

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -1,49 +1,54 @@
 import {BookInterface} from '../models/book';
-import {Dayactivity, Dayovertime, Timesheet, TIMESHEETS} from '../models/timesheet';
+import {Dayactivity, Dayovertime, Timesheet} from '../models/timesheet';
 import {map} from 'rxjs/internal/operators';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
 import {Product} from '../models/products';
 import {HttpClient} from '@angular/common/http';
+import {v4 as uuidv4} from 'uuid';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataApiService {
 
-
   constructor(private afs: AngularFirestore,
               private http: HttpClient) {
   }
 
-
+  private timesheet: Observable<Timesheet>;
+  private timesheetList: Observable<Timesheet[]>;
+  private timesheetsCollection: AngularFirestoreCollection<Timesheet>;
+  private timesheetDoc: AngularFirestoreDocument<Timesheet>;
+  public temporaryTimesheet: Timesheet = {
+    weekhoursplanned: 0,
+    statusmanager: {},
+    weekactivities: [],
+    id: null,
+    userUid: null
+  };
+  public selectedDayActivity: Dayactivity = {id: null};
+  public selectedDayOvertime: Dayovertime = {overtime: 0, day: null};
+  public timesheets: Observable<Timesheet[]>;
+  selectedActRow: number;
+  selectedOtRow: number;
 
   private booksCollection: AngularFirestoreCollection<BookInterface>;
-  private timesheetsCollection: AngularFirestoreCollection<Timesheet>;
   private booksList: Observable<BookInterface[]>;
   private books: Observable<BookInterface[]>;
   private booksOffers: Observable<BookInterface[]>;
   private bookDoc: AngularFirestoreDocument<BookInterface>;
   private book: Observable<BookInterface>;
   public selectedBook: BookInterface = {id: null};
+  public selectedTimesheet: Timesheet = {weekactivities: [], id: null};
   public selectedProduct: Product = {id: null};
-  public temporaryTimesheet: Timesheet = {statusmanager: undefined, weekactivities: [], id: null, userUid : null};
-  public selectedDayActivity: Dayactivity = {id: null};
-  public selectedDayOvertime: Dayovertime = {day: null};
-  public timesheets: Timesheet[] = TIMESHEETS;
-  selectedActRow: number;
-  selectedOtRow: number;
+
   selectedRow: number;
 
-  getMyTimesheets(user) {
-    console.log('Timesheets fromn api ' + JSON.stringify(this.timesheets));
-    return this.timesheets;
-  }
 
-  updateMyTimesheet(id, timesheet) {
-
-  }
+// -----------------------------------------------------------------------
 
   getMyTimesheetsJSON() {
     return this.http.get<any>('assets/timesheets.json')
@@ -79,7 +84,6 @@ export class DataApiService {
       }));
   }
 
-
   getOneBook(idBook: string) {
     this.bookDoc = this.afs.doc<BookInterface>(`books/${idBook}`);
     return this.book = this.bookDoc
@@ -108,35 +112,15 @@ export class DataApiService {
       }));
   }
 
-
-
-  addBook(book: BookInterface) {
-    return new Promise((resolve, reject) => {
-
-      this.booksCollection.add(book)
-        .then(userData => {
-        }).catch(err => console.log(reject(err)));
-
-    });
-  }
-
-  addTimesheet(timesheet: Timesheet) {
-    return new Promise((resolve, reject) => {
-
-      this.timesheetsCollection.add(timesheet)
-        .then(userData => {
-        }).catch(err => console.log(reject(err)));
-
-    });
-  }
-
   updateBook(book: BookInterface) {
     let idBook = book.id;
     this.bookDoc = this.afs.doc<BookInterface>(`books/${idBook}`);
-    return this.bookDoc.update(book).then((res) => {
-    }).catch(err => {
-      console.log('err', err.message);
-    });
+    return this.bookDoc.update(book)
+      .then((res) => {
+      })
+      .catch(err => {
+        console.log('err', err.message);
+      });
   }
 
   deleteBook(idBook: string) {
@@ -147,8 +131,89 @@ export class DataApiService {
     });
   }
 
-  createNewtemporaryTimesheet(timesheet : Timesheet) {
-    console.log("Temporary timesheet created :"+ timesheet)
-    //this.temporaryTimesheet= timesheet;
+  addBook(book: BookInterface) {
+    let uuid = uuidv4();
+    book.id = uuid;
+    return new Promise((resolve, reject) => {
+      this.booksCollection.add(book)
+        .then(userData => {
+        })
+        .catch(err => console.log(reject(err)));
+    });
   }
+
+//---------------------------------------------------------------------------
+
+  deleteTimesheet(idTimesheet: string) {
+    this.timesheetDoc = this.afs.doc<Timesheet>(`timesheets/${idTimesheet}`);
+    return this.timesheetDoc.delete().then((res) => {
+    }).catch(err => {
+      console.log('err', err.message);
+    });
+  }
+
+  getMyTimesheets(user) {
+    this.timesheetsCollection = this.afs.collection<Timesheet>('timesheets');
+    return this.timesheetList = this.timesheetsCollection.snapshotChanges()
+      .pipe(map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Timesheet;
+          data.id = action.payload.doc.data().id;
+          return data;
+        }).filter(data => data.userUid == user);
+      }));
+  }
+
+  updateTimesheet() {
+    let idTimesheet = this.temporaryTimesheet.id;
+    this.timesheetDoc = this.afs.doc<Timesheet>(`timesheets/${idTimesheet}`);
+    return this.timesheetDoc.update(this.temporaryTimesheet)
+      .then((res) => {
+      })
+      .catch(err => {
+        console.log('err', err.message);
+      });
+  }
+
+  createNewTimesheet(timesheet: Timesheet) {
+    // return    this.afs.collection('timesheets').add(timesheet).then(function(docRef) {
+    //   return docRef.id;
+    // });
+    let uuid = uuidv4();
+    timesheet.id = uuid;
+    return new Promise((resolve, reject) => {
+      this.timesheetsCollection.add(timesheet)
+        .then(userData => {
+          console.log('success');
+        })
+        .catch(err => console.log(reject(err)));
+    });
+  }
+
+
+  createNewtemporaryTimesheet(timesheet: Timesheet) {
+    this.temporaryTimesheet = {...this.temporaryTimesheet, ...timesheet};
+    console.log('Temporary timesheet created :' + JSON.stringify(this.temporaryTimesheet));
+  }
+
+  createNewActivity(activity: Dayactivity) {
+    console.log('new activity created :' + JSON.stringify(activity));
+    console.log('current timesheet :' + JSON.stringify(this.temporaryTimesheet));
+    console.log('current timesheet acts :' + JSON.stringify(this.temporaryTimesheet.weekactivities));
+    this.temporaryTimesheet.weekactivities?.push(activity);
+  }
+
+  getAllTimesheets() {
+    this.timesheetsCollection = this.afs.collection<Timesheet>('timesheets');
+    return this.timesheets = this.timesheetsCollection
+      .snapshotChanges()
+      .pipe(map(changes => {
+        return changes.map(action => {
+          const data = action.payload.doc.data() as Timesheet;
+          data.id = action.payload.doc.data().id;
+          return data;
+        });
+      }));
+  }
+
 }
